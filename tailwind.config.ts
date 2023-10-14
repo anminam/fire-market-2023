f7ff5de64ad5029edded7b31f6faff4885e40de2
anminam <minamhunmin@gmail.com>
2023-10-14 15:26:16 +0900

    [feat] ui 라이브러리 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+
+const plugin = require('tailwindcss/plugin');
 
 const config: Config = {
   content: [
@@ -15,6 +17,33 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: false, // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: 'dark', // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: false, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+    prefix: '', // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+  },
+  plugins1: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        '.btn': {
+          padding: '.5rem 1rem !important',
+          borderRadius: '.25rem !important',
+          fontWeight: '600 !important',
+          backgroundColor: '#fff !important',
+        },
+        '.btn-neutral': {
+          backgroundColor: 'transparent !important',
+          color: '#fff !important',
+          borderColor: '#fff !important',
+        },
+      });
+    }),
+  ],
+};
+export default config;
