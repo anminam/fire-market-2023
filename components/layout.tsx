51f140b22bee02725880f7b8e0922c0d93524987
anminam <minamhunmin@gmail.com>
2023-10-14 15:28:03 +0900

    [style] 뒤로가기 아이콘 변경

--- components/layout.tsx
+++ components/layout.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import LayoutTitle from './layout-title';
 import GroundNavBar from './GroundNavBar';
 import Head from 'next/head';
+import { BiSolidChevronLeft } from 'react-icons/bi';
 
 interface LayoutProps {
   title?: string;
@@ -30,23 +31,10 @@ export default function Layout({
       <Head>
         <title>{title}</title>
       </Head>
-      <div className="bg-white w-full h-12 max-w-xl justify-center text-lg px-10 font-medium fixed text-gray-800 border-b top-0  flex items-center z-10">
+      <div className="w-full h-12 max-w-xl justify-center text-lg px-10 font-medium fixed border-b top-0 flex items-center z-10">
         {canGoBack ? (
           <button onClick={onClick} className="absolute left-4">
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 19l-7-7 7-7"
-              ></path>
-            </svg>
+            <BiSolidChevronLeft />
           </button>
         ) : null}
         {pTitle && <LayoutTitle canGoBack={canGoBack}>{pTitle}</LayoutTitle>}
