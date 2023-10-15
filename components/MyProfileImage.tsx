99819cffe14d9e50a8d46366670eb7b7d3ae6216
anminam <minamhunmin@gmail.com>
2023-10-15 11:44:38 +0900

    [refactor] 프로필 이미지

--- components/MyProfileImage.tsx
+++ components/MyProfileImage.tsx
@@ -5,14 +5,14 @@ const MyProfileImage = () => {
   const { user } = useUser();
   return (
     <div className="avatar">
-      <div className="w-16 rounded-full">
+      <div className="w-16 h-16 rounded-full bg-slate-500 relative overflow-hidden">
         {user?.avatar && (
           <Image
-            height={64}
-            width={64}
             alt="avatar"
             src={`https://imagedelivery.net/6-jfB1-8fzgOcmfBEr6cGA/${user?.avatar}/avatar`}
-            className="w-16 h-16 bg-slate-500 rounded-full"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority={true}
           />
         )}
       </div>
