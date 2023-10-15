c88f52f3299fba10a3aedab7ceafde006baa961d
anminam <minamhunmin@gmail.com>
2023-10-15 21:28:35 +0900

    [feat] add api 상품가져오기용

---
+++ pages/api/users/my/sales-all.ts
@@ -0,0 +1,30 @@
+import client from '@/libs/server/client';
+import withHandlers from '@/libs/server/withHandlers';
+import { withApiSession } from '@/libs/server/withSession';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    session: { user },
+  } = req;
+  const products = await client.product.findMany({
+    where: {
+      user: {
+        id: user?.id,
+      },
+    },
+    include: {
+      _count: {
+        select: {
+          Favorite: true,
+        },
+      },
+    },
+  });
+  res.json({
+    result: true,
+    products,
+  });
+}
+
+export default withApiSession(withHandlers({ methods: ['GET'], handler }));
