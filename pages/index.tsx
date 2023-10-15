b7403b505439fa5d9f7eb429899cebc8e1db9324
anminam <minamhunmin@gmail.com>
2023-10-15 11:43:37 +0900

    [refactor] 메인상품 컨테이너 나누기

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from 'next';
 import FloatingButton from '@/components/floating-button';
-import Item from '@/components/ProductItem';
 import Layout from '@/components/layout';
-
 import useUser from '@/libs/client/useUser';
 import useSWR, { SWRConfig } from 'swr';
 import { Product } from '@prisma/client';
-import Image from 'next/image';
-import 쿠로미 from '@/public/쿠로미.png';
 import client from '@/libs/server/client';
-import { AiOutlinePlus } from 'react-icons/ai';
 import { HiPlus } from 'react-icons/hi';
+import MainProducts from '@/components/MainProducts';
 
 interface ProductsResponse {
   result: boolean;
@@ -30,19 +26,7 @@ const Home: NextPage = () => {
   return (
     <Layout title="장터보기" hasTabBar>
       <div className="flex flex-col space-y-5 divide-y divide-neutral">
-        {data
-          ? data?.products.map((product) => (
-              <Item
-                id={product.id}
-                key={product.id}
-                title={product.name}
-                price={product.price}
-                comments={1}
-                hearts={product._count?.Favorite || 0}
-                imgSrc={product.image}
-              />
-            ))
-          : 'loading'}
+        {data ? <MainProducts products={data.products} /> : 'loading'}
 
         <FloatingButton href="/products/upload">
           <HiPlus className="text-lg" />
