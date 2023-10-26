b183955b63ba25ac1bd0318d0dfdd118af8aaa81
hunmin.ahn <hunmin.ahn@samsung.com>
2023-10-26 12:30:28 +0900

    [refactor] add comma in money input

--- components/input.tsx
+++ components/input.tsx
@@ -1,15 +1,18 @@
+import { useState } from "react";
+import { NumericFormat } from "react-number-format";
+
 interface InputProps {
   label: string;
   name: string;
   register: any;
-  kind?: 'text' | 'phone' | 'price';
+  kind?: "text" | "phone" | "price";
   [key: string]: any;
 }
 
 export default function Input({
   label,
   name,
-  kind = 'text',
+  kind = "text",
   register,
   type,
   required,
@@ -20,7 +23,7 @@ export default function Input({
       <label className="text-sm font-bold" htmlFor={name}>
         {label}
       </label>
-      {kind === 'text' ? (
+      {kind === "text" ? (
         <div className="rounded-md relative flex items-center shadow-sm">
           <input
             id={name}
@@ -30,25 +33,41 @@ export default function Input({
           />
         </div>
       ) : null}
-      {kind === 'price' ? (
-        <div className="rounded-md relative flex items-center shadow-sm">
-          <div className="absolute left-0 pointer-events-none pl-3 flex items-center justify-center">
+      {kind === "price" ? (
+        <div className="relative">
+          <div className="absolute left-0 pointer-events-none pl-3 top-[14px] flex items-center justify-center">
             <span className="text-gray-500 text-sm">₩</span>
           </div>
-          <input
+          <NumericFormat
             id={name}
             required={required}
-            type={type}
+            type="tel"
+            className="appearance-none input input-bordered w-full placeholder-neutral-600 pl-7"
             {...register}
             {...rest}
-            className="appearance-none pl-7 input input-bordered w-full placeholder-neutral-600"
+            allowLeadingZeros
+            thousandSeparator=","
           />
-          <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
-            <span className="text-gray-500">원</span>
-          </div>
         </div>
-      ) : null}
-      {kind === 'phone' ? (
+      ) : // <div className="rounded-md relative flex items-center shadow-sm">
+      //   <div className="absolute left-0 pointer-events-none pl-3 flex items-center justify-center">
+      //     <span className="text-gray-500 text-sm">₩</span>
+      //   </div>
+      //   <input
+      //     id={name}
+      //     required={required}
+      //     type={type}
+      //     {...register}
+      //     {...rest}
+      //     onChange={handleOnChanged}
+      //     className="appearance-none pl-7 input input-bordered w-full placeholder-neutral-600"
+      //   />
+      //   <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
+      //     <span className="text-gray-500">원</span>
+      //   </div>
+      // </div>
+      null}
+      {kind === "phone" ? (
         <div className="flex rounded-md shadow-sm">
           <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">
             +82
