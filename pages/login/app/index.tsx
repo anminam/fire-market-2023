6a75ae1f9215b3baa1673825faa3df63e68ea6e6
Daniel Lim <lahuman@daum.net>
2023-10-28 16:31:38 +0900

    feat : app login

--- pages/login/app/index.tsx
+++ pages/login/app/index.tsx
@@ -1,11 +1,17 @@
+import { useMiniStore } from '@/hooks/useStore';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const AppLoginPage = () => {
   const router = useRouter();
+  const { setToken, setUserId } = useMiniStore();
 
   useEffect(() => {
-    console.log(router.data);
+    console.log(router.query.data);
+    const data = JSON.parse(atob(router.query.data));
+    console.log(data.id);
+    setToken(data.token);
+    setUserId(data.id);
     router.replace('/');
   }, [router]);
 
