5e5a4ef78e81bce1fdc0e2596ba1232e0ad2a714
anminam <minamhunmin@gmail.com>
2023-10-28 17:24:04 +0900

    [update] add query type

--- pages/login/app/index.tsx
+++ pages/login/app/index.tsx
@@ -8,12 +8,12 @@ const AppLoginPage = () => {
 
   useEffect(() => {
     console.log(router.query.data);
-    const data = JSON.parse(atob(router.query.data));
+    const data = JSON.parse(atob(router.query.data as string));
     console.log(data.id);
     setToken(data.token);
     setUserId(data.id);
     router.replace('/');
-  }, [router]);
+  }, [router, setToken, setUserId]);
 
   return <div>로그인 중입니다...</div>;
 };
