9976a791076baf73102bdfd0287dc96b98e0d6fc
anminam <minamhunmin@gmail.com>
2023-10-30 23:14:55 +0900

    [refactor] 판매내역 상품리스트 변경

--- components/ProductByList.tsx
+++ components/ProductByList.tsx
@@ -5,30 +5,28 @@ import NothingWithContainer from './NothingWithContainer';
 interface IProps {
   list: Product[];
 }
-function CenterContainer({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="flex justify-center items-center h-full w-full mt-10">
-      {children}
-    </div>
-  );
-}
+
 export default function ProductByList({ list }: IProps) {
   if (!list.length) {
     return <NothingWithContainer />;
   }
 
-  return list.map(_ => {
-    return (
-      <Item
-        id={_.id}
-        key={_.id}
-        title={_.name}
-        price={_.price}
-        imgSrc={_.image}
-        comments={1}
-        // hearts={_.Favorite}
-        status={_.statusCd}
-      />
-    );
-  });
+  return (
+    <div>
+      {list.map((_) => {
+        return (
+          <Item
+            id={_.id}
+            key={_.id}
+            title={_.name}
+            price={_.price}
+            imgSrc={_.image}
+            comments={1}
+            // hearts={_.Favorite}
+            status={_.statusCd}
+          />
+        );
+      })}
+    </div>
+  );
 }
--- pages/myfire/sell.tsx
+++ pages/myfire/sell.tsx
@@ -2,45 +2,32 @@ import type { NextPage } from 'next';
 import Layout from '@/components/layout';
 
 import useSWR from 'swr';
-import useUser from '@/libs/client/useUser';
-import { Product } from '@prisma/client';
 import { useEffect, useState } from 'react';
 import { cls } from '@/libs/client/utils';
-import ProductByList from '@/components/ProductByList';
 import { ProductStatus } from '@/interface/ProductKind';
+import MainProducts from '@/components/MainProducts';
+import { ProductWithCount } from '@/interface/Product';
 
 interface ProductListResponse {
   result: boolean;
-  data: Product[];
+  data: ProductWithCount[];
 }
 
-// interface SellItem {
-//   label: string;
-//   list: Product[];
-// }
-
-// const list: SellItem[] = [
-//   { label: '판매중', list: [] },
-//   { label: '거래완료', list: [] },
-//   { label: '숨김', list: [] },
-// ];
-
 const Sell: NextPage = () => {
-  const { user } = useUser();
   const { data, isLoading } = useSWR<ProductListResponse>(`/api/product/user/`);
 
-  const [list0, setList0] = useState<Product[]>([]);
-  const [list1, setList1] = useState<Product[]>([]);
-  const [list2, setList2] = useState<Product[]>([]);
+  const [list0, setList0] = useState<ProductWithCount[]>([]);
+  const [list1, setList1] = useState<ProductWithCount[]>([]);
+  const [list2, setList2] = useState<ProductWithCount[]>([]);
 
   const [tab, setTab] = useState(0);
 
   useEffect(() => {
-    const list0: Product[] = [];
-    const list1: Product[] = [];
-    const list2: Product[] = [];
+    const list0: ProductWithCount[] = [];
+    const list1: ProductWithCount[] = [];
+    const list2: ProductWithCount[] = [];
     if (data) {
-      data.data.forEach(_ => {
+      data.data.forEach((_) => {
         switch (_.statusCd) {
           case ProductStatus.SALE:
           case ProductStatus.RSRV:
@@ -65,36 +52,27 @@ const Sell: NextPage = () => {
       <div className="">
         <div className="tabs flex text-sm">
           <a
-            className={cls(
-              `tab tab-bordered tab-lg flex-1 mt text-sm`,
-              tab === 0 ? 'tab-active' : ''
-            )}
+            className={cls(`tab tab-bordered tab-lg flex-1 mt text-sm`, tab === 0 ? 'tab-active' : '')}
             onClick={() => setTab(0)}
           >
             판매중 {list0.length ? list0.length : ''}
           </a>
           <a
-            className={cls(
-              `tab tab-bordered tab-lg flex-1 mt text-sm`,
-              tab === 1 ? 'tab-active' : ''
-            )}
+            className={cls(`tab tab-bordered tab-lg flex-1 mt text-sm`, tab === 1 ? 'tab-active' : '')}
             onClick={() => setTab(1)}
           >
             거래완료 {list1.length ? list1.length : ''}
           </a>
           <a
-            className={cls(
-              `tab tab-bordered tab-lg flex-1 mt text-sm`,
-              tab === 2 ? 'tab-active' : ''
-            )}
+            className={cls(`tab tab-bordered tab-lg flex-1 mt text-sm`, tab === 2 ? 'tab-active' : '')}
             onClick={() => setTab(2)}
           >
             숨김 {list2.length ? list2.length : ''}
           </a>
         </div>
-        {tab === 0 && <ProductByList list={list0} />}
-        {tab === 1 && <ProductByList list={list1} />}
-        {tab === 2 && <ProductByList list={list2} />}
+        {tab === 0 && <MainProducts products={list0} />}
+        {tab === 1 && <MainProducts products={list1} />}
+        {tab === 2 && <MainProducts products={list2} />}
       </div>
       <div></div>
     </Layout>
